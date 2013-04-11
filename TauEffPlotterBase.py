d13864468dfd349525c25f55c67c740cc69f8cc8
Mauro Verzetti <mverzett@cern.ch>
2013-04-11 03:00:30 -0500

    added systematic uncertainties

--- TauEffPlotterBase.py
+++ TauEffPlotterBase.py
@@ -20,6 +20,7 @@ from FinalStateAnalysis.PlotTools.PoissonView import PoissonView
 from FinalStateAnalysis.PlotTools.HistToTGRaphErrors import HistToTGRaphErrors, HistStackToTGRaphErrors
 from FinalStateAnalysis.PlotTools.InflateErrorView import InflateErrorView
 from FinalStateAnalysis.MetaData.data_styles import data_styles
+import shelve
 #from RecoLuminosity.LumiDB import argparse''' 
 import sys
 import os
@@ -28,6 +29,7 @@ import pprint
 import ROOT
 import fnmatch
 import math
+import systematics
 
 ROOT.gROOT.SetBatch(True)
 ROOT.gStyle.SetOptTitle(0)
@@ -58,12 +60,21 @@ class TauEffPlotterBase(Plotter):
         self.shape_systematics = []
         #expressed in %
         self.scale_systematics = {
+            #key gets matched to the MC names
             '*' : {
-                'lumi' : 0.,
+                #'*lumi'  : systematics.luminosity,
+                'trigger': systematics.mu_id_and_trigger,
                 },
-            'Z + jets' : {
-                'xsec' : 0.,
-                }
+            'Z*mumu' : {
+                'xsec' : systematics.Z_xsection,
+                },
+            'QCD' : {
+                'os_ss_ratio' : systematics.qcd_extrapolation,
+                },
+            'ttbar' : {
+                'xsec' : systematics.ttbar_xsection,
+                'btag_eff' : systematics.btag_efficiency,
+                },
             }
 
     def get_view(self, *args): #Is it against Liskov Substitution Principle? I don't care
@@ -85,8 +96,9 @@ class TauEffPlotterBase(Plotter):
             self.canvas.SetLogy()
         self.add_cms_blurb(self.sqrts)
 
-    def write_summary(self, iso_name, variable):
+    def write_summary(self, iso_name, variable, shape_only=False):
         ''' Write final cut-and-count table of events passing the selection '''
+        shelf     = shelve.open( os.path.join(self.outputdir,'summary_table_%s.raw_txt' % variable) )
         sys_name  = self.systematic
         all_views = self.get_signal_views(iso_name, variable)
         sys_views = {}
@@ -107,22 +119,37 @@ class TauEffPlotterBase(Plotter):
         for name, view in all_views.iteritems():
             hist    = view.Get(variable)
             n_events= hist.GetBinContent(1)
+            stat_err= hist.GetBinError(1)
             #compute nevents shape_systematics
             shape_sys_events = [sys_views[sys][name].Get(variable).GetBinContent(1) for sys in self.shape_systematics]
             #makes abs difference
-            shape_sys_diffs  = [abs(i-n_events) for i in shape_sys_events]
+            sys_errors       = [abs(i-n_events) for i in shape_sys_events]
+            if not shape_only:
+                for key, item in self.scale_systematics.iteritems():
+                    if fnmatch.fnmatch(name,key):
+                        for sys_name, sys_effect in item.iteritems():
+                            print 'applying %s: %s to %s' % (key, sys_name, name)
+                            sys_errors.append(n_events*sys_effect)
+                    
             #sum in quad
             sys_err = 0.
-            if len(shape_sys_diffs):
-                sys_err = quad(*shape_sys_diffs)
+            if len(sys_errors):
+                sys_err = quad(*sys_errors)
             if name == 'data':
                 output += tab_form % ('Bkg. Sum', '%.1f' % sum(expected_evts['exp' ]), '%.1f' % quad(*expected_evts['stat']), '%.1f' % quad(*expected_evts['sys' ]))
+                sys_err = 0
             else:
                 expected_evts['exp' ].append(n_events)
-                expected_evts['stat'].append(hist.GetBinError(1))
+                expected_evts['stat'].append(stat_err)
                 expected_evts['sys' ].append(sys_err)
+            shelf[name] = {
+                'nevts' : n_events,
+                'stat'  : stat_err,
+                'sys'   : sys_err,
+                }
             output += tab_form % (name, '%.1f' % n_events, '%.1f' % hist.GetBinError(1), '%.1f' % sys_err)
-            
-        with open(os.path.join(self.outputdir,'summary_table_%s.raw_txt' % variable),'w') as out_file:
+
+        shelf.close()
+        with open(os.path.join(self.outputdir,'summary_table_%s' % variable),'w') as out_file:
             out_file.write(output)
 
