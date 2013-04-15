10b584f3dfcf4faf222346fc0aa3c6a567982f66
Mauro Verzetti <mverzett@cern.ch>
2013-04-15 03:48:41 -0500

    made qcd/wjets extrapolation more flexible with respect to the MT sideband to be used, resuffled output directory names

--- TauEffPlotterMT.py
+++ TauEffPlotterMT.py
@@ -67,7 +67,7 @@ class TauEffPlotterMT(TauEffPlotterBase):
             'Z#rightarrow#mu#mu'
             )
 
-    def compute_contributions(self, iso_name):
+    def compute_contributions(self, iso_name, himtregion):
         '''uses ABCD method to extrapolate the WJets and QCD yield in SS region. Regions:
         A - AntiIso objects LowMt
         B - AntiIso objects HihgMt
@@ -77,16 +77,17 @@ class TauEffPlotterMT(TauEffPlotterBase):
         wjet = self.get_view('WplusJets*')
         
         qcd_ss_noIso_loMt = data.Get('ss/QCD/LoMT/mPt').Integral()
-        qcd_ss_noIso_hiMt = data.Get('ss/QCD/HiMT/mPt').Integral()
+        qcd_ss_noIso_hiMt = data.Get('ss/QCD/%s/mPt' % himtregion).Integral()
         qcd_ratio_lo_hi_mt= qcd_ss_noIso_loMt / qcd_ss_noIso_hiMt
 
         data_ss_Iso_loMt = data.Get('%s/ss/LoMT/mPt' % iso_name).Integral()
-        data_ss_Iso_hiMt = data.Get('%s/ss/HiMT/mPt' % iso_name).Integral()
+        data_ss_Iso_hiMt = data.Get('%s/ss/%s/mPt' % (iso_name, himtregion)).Integral()
         
         wjet_ss_Iso_loMt = wjet.Get('%s/ss/LoMT/mPt' % iso_name).Integral()
-        wjet_ss_Iso_hiMt = wjet.Get('%s/ss/HiMT/mPt' % iso_name).Integral()
+        wjet_ss_Iso_hiMt = wjet.Get('%s/ss/%s/mPt' % (iso_name, himtregion)).Integral()
         w_ratio_lo_hi_mt = wjet_ss_Iso_loMt / wjet_ss_Iso_hiMt
 
+        print himtregion, data_ss_Iso_hiMt, w_ratio_lo_hi_mt, data_ss_Iso_loMt, (w_ratio_lo_hi_mt - qcd_ratio_lo_hi_mt)
         qcd_yield_hiMt = (data_ss_Iso_hiMt*w_ratio_lo_hi_mt - data_ss_Iso_loMt)/(w_ratio_lo_hi_mt - qcd_ratio_lo_hi_mt)
         qcd_yield_loMt = qcd_yield_hiMt*qcd_ratio_lo_hi_mt
 
@@ -105,7 +106,7 @@ class TauEffPlotterMT(TauEffPlotterBase):
                         , **data_styles['QCD*']),
                         'QCD'),                        
                 },
-            'HiMT' : {
+            himtregion : {
                 'yield' : qcd_yield_hiMt,
                 'view'  : views.TitleView(
                     views.StyleView(
@@ -124,13 +125,13 @@ class TauEffPlotterMT(TauEffPlotterBase):
         
         
         
-    def get_w_estimation(self, iso_name, sign_region='os'):
+    def get_w_estimation(self, iso_name, mtregion, sign_region='os'):
         mt_h_name= 'mMtToPfMet_Ty1'
-        mtregion = 'HiMT'
+        #mtregion = 'HiMT'
         wjet_reg =  '/'.join((iso_name, sign_region, mtregion,''))
 
         #qcd contribution
-        qcd_view = self.compute_contributions(iso_name)['HiMT']['view']
+        qcd_view = self.compute_contributions(iso_name, mtregion)[mtregion]['view']
         qcd_view = views.ScaleView(qcd_view, -1)
         
         #Scale factor
@@ -153,7 +154,7 @@ class TauEffPlotterMT(TauEffPlotterBase):
         wjet_est = views.TitleView(wjet_est, 'WplusJets')
         return wjet_est
 
-    def get_qcd_estimation(self, iso_name):
+    def get_qcd_estimation(self, iso_name, mtregion):
         qcd_reg   = '%s/ss/LoMT/' % iso_name
 
         #MC ZJets view
@@ -161,7 +162,7 @@ class TauEffPlotterMT(TauEffPlotterBase):
         neg_zjet = views.SubdirectoryView(neg_zjet,qcd_reg)
 
         #DATA WJets estimation
-        neg_wjet = views.ScaleView(self.get_w_estimation(iso_name, 'ss'), -1)
+        neg_wjet = views.ScaleView(self.get_w_estimation(iso_name, mtregion, 'ss'), -1)
         
         #Data view
         data_view= views.SubdirectoryView(self.get_view('data'),qcd_reg)
@@ -172,7 +173,7 @@ class TauEffPlotterMT(TauEffPlotterBase):
         qcd_est  = views.TitleView(qcd_est, 'QCD')
         return qcd_est
 
-    def make_folder_views(self, folder, rebin):
+    def make_folder_views(self, folder, rebin, mtregion='HiMT'):
         iso_name    = folder.split('/')[0]
         zjets_mc    = self.get_view_dir('Zjets_M50'  , rebin, folder)
         data        = self.get_view_dir('data'       , rebin, folder)
@@ -198,10 +199,10 @@ class TauEffPlotterMT(TauEffPlotterBase):
             )
                 
         #makes WJets Estimation view
-        wjet_est  = self.rebin_view(self.get_w_estimation(iso_name), rebin)
+        wjet_est  = self.rebin_view(self.get_w_estimation(iso_name, mtregion), rebin)
         
         #makes QCD Estimation view
-        qcd_est   = self.rebin_view(self.get_qcd_estimation(iso_name), rebin)
+        qcd_est   = self.rebin_view(self.get_qcd_estimation(iso_name, mtregion), rebin)
         return {
             'Z_tautau' : zjets_mc,
             'Z_mumu'   : zjets_mm_mc,
@@ -215,12 +216,13 @@ class TauEffPlotterMT(TauEffPlotterBase):
     def plot_wjets_region(self, folder, variable, rebin=1, xaxis='', leftside=True,
                         xrange=None, logscale=False, **kwargs):
         iso_name    = folder.split('/')[0]
+        mtregion    = folder.split('/')[-1]
         zjets_mc    = self.get_view_dir('Zjets_M50'  , rebin, folder)
         data        = self.get_view_dir('data'       , rebin, folder)
         ttbar       = self.get_view_dir('TTplusJets*', rebin, folder)
         zjets_mm_mc = self.get_view_dir('Zjets_ZToMuMu_M50'  , rebin, folder)
         wjets_mc    = self.get_view_dir('WplusJets*',  rebin, folder)
-        qcd_est     = self.rebin_view(self.compute_contributions(iso_name)['HiMT']['view'], rebin)
+        qcd_est     = self.rebin_view(self.compute_contributions(iso_name, mtregion)[mtregion]['view'], rebin)
         diboson     = views.TitleView(
             views.StyleView(
                 views.SumView(
@@ -316,6 +318,7 @@ plotter = TauEffPlotterMT()
 print '\n\nPlotting MT\n\n'
 
 #pprint.pprint(plotter.views)
+
 ids =  [
             'LooseIso'    ,
             'MediumIso'   ,
@@ -334,22 +337,38 @@ ids =  [
 
 #Make Ztt WJets and TTbar region plots
 for iso in ids:
-    for region in ['LoMT','HiMT']:
-        for var, kwargs in toPlot.iteritems():
-            folder = '/'.join([iso,'os',region])
-            plotter.set_subdir(folder)
-            plotter.plot_mc_vs_data(folder, var, **kwargs)
-            plotter.save('mc_vs_data_os_%s_%s_%s' % (iso, region, var) )
-            if 'HiMT' in folder:
-                plotter.plot_wjets_region(folder, var, **kwargs)
-                plotter.save('final_os_%s_%s_%s' % (iso, region, var) )
-            if 'os/LoMT' in folder:
-                plotter.plot_with_estimate(folder, var, **kwargs)
-                plotter.save('final_data_os_%s_%s_%s' % (iso, region, var) )
+    region = 'LoMT'
+    for var, kwargs in toPlot.iteritems():
+        folder = '/'.join([iso,'os',region])
+        plotter.set_subdir(os.path.join(iso, 'signal'))
+        plotter.plot_mc_vs_data(folder, var, **kwargs)
+        plotter.save('mc_vs_data_os_%s_%s_%s' % (iso, region, var) )
+        print 'plot_with_estimate: ',folder, var
+        plotter.plot_with_estimate(folder, var, **kwargs)
+        plotter.save('final_data_os_%s_%s_%s' % (iso, region, var) )
     plotter.set_subdir(iso)
     plotter.write_summary(iso,'m_t_Mass')
     #plotter.write_summary(iso,'mAbsEta')
-            
+
+    for region, dirname in [
+            ('HiMT', 'MT_Greater_40'),
+            ('VHiMT', 'MT_Greater_70'),
+            ('MT70_120', 'MT_70_120'),
+            ]:
+        for var, kwargs in toPlot.iteritems():
+            plotter.set_subdir(os.path.join(iso, dirname))
+            folder = '/'.join([iso,'os',region])
+            plotter.plot_mc_vs_data(folder, var, **kwargs)
+            plotter.save('mc_vs_data_os_%s_%s_%s' % (iso, region, var) )
+            print 'plot_wjets_region: ',folder, var
+            plotter.plot_wjets_region(folder, var, **kwargs)
+            plotter.save('final_os_%s_%s_%s' % (iso, region, var) )
+
+
+plotter.set_subdir('')
+#plotter.write_json_dump('m_t_Mass')
+
+
 #Make QCD region plots
 for iso in ids:
     region = 'LoMT'
@@ -364,29 +383,6 @@ integral_os = sum([plotter.views['data']['view'].Get('NOSYS/os/QCD/%s/mPt' % m).
 ratio_ss_os = integral_ss/integral_os
 
 print '(ss = %.1f +- %.1f) / (os = %.1f +- %.1f) = %.4f +- %.4f' % (integral_ss, math.sqrt(integral_ss), integral_os, math.sqrt(integral_os), ratio_ss_os, ratio_ss_os*quad(1./math.sqrt(integral_ss)+1./math.sqrt(integral_os)))
-
-#Make WJets region plots
-for iso in ids:
-    region = 'HiMT'
-    for var, kwargs in toPlot.iteritems():
-        folder = '/'.join([iso,'ss',region])
-        plotter.set_subdir('/'.join([iso,'wjets']))
-        plotter.plot_mc_vs_data(folder, var, **kwargs)
-        plotter.save('mc_vs_data_os_%s_%s_%s' % (iso, region, var) )
-        plotter.plot_wjets_region(folder, var, **kwargs)
-        plotter.save('final_os_%s_%s_%s' % (iso, region, var) )
-
-## for syst in ["RAW"]+[i+j for i,j in itertools.product(['mes','tes','jes','ues'],['_p'])]:
-##     plotter.systematic = syst
-##     for iso in ids:
-##         folder = '/'.join([iso,syst])
-##         plotter.set_subdir(folder)
-##         region = 'LoMT'
-##         for var, kwargs in toPlot.iteritems():
-##             folder = '/'.join([iso,'os',region])
-##             plotter.plot_with_estimate(folder, var, **kwargs)
-##             plotter.save('final_data_os_%s_%s_%s' % (iso, region, var) )
-##         plotter.write_summary(iso,'m_t_Mass')
 
 
 #FIXME: _understand systamtic uncertainties:
