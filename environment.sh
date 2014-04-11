698e01eebfebb197ddb8460f8410486571f2777f
Mauro Verzetti <mverzett@cern.ch>
2014-04-11 04:28:28 -0500

    add MEGAPATH to work in batch

--- environment.sh
+++ environment.sh
@@ -4,6 +4,7 @@ echo "Setting up CMSSW runtime environment"
 eval `scramv1 ru -sh`
 source $CMSSW_BASE/src/FinalStateAnalysis/environment.sh
 
+export MEGAPATH=$hdfs
 source jobid.sh
 export jobid=$jobid8
 export pub=$HOME/public_html/tauIdEffMeas/$jobid/
